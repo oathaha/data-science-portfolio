208332ec6eb7dbccd4dd6e7319221a61efccc725
oathaha <windowsandandroid.app@gmail.com>
2024-07-30 20:50:32 +1000

    refactor code for training + create code for inference

--- Medium-Articles-title-generation/script/generate.py
+++ Medium-Articles-title-generation/script/generate.py
@@ -1,40 +1,46 @@
 import os, argparse
 
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoModelForSeq2SeqLM
 
 from peft import PeftModel
 
 from datasets import load_dataset
 
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--model_name', type = str, default = 'llama2-7b')
+parser.add_argument('--ckpt_dir', type = str, required=True)
 
 args = parser.parse_args()
 
 #%%
 
 model_names = {
-    'llama2-7b': 'meta-llama/Llama-2-7b',
-    'mistral-7b': 'mistralai/Mistral-7B-v0.3'
+    'llama2-7b': 'meta-llama/Llama-2-7b-hf',
+    'mistral-7b': 'mistralai/Mistral-7B-v0.3',
+    't5': 'google-t5/t5-3b',
+    'bart': 'facebook/bart-large'
 }
 
 data_dir = '../dataset/cleaned/'
 model_name_arg = args.model_name
 model_name = model_names.get(model_name_arg, '')
-new_model_name = '' ## will add later...
+ckpt_dir = args.ckpt_dir
 
-output_model_dir = '../fine-tuned-model/'
-real_model_path = os.path.join(output_model_dir, new_model_name)
+output_model_dir = '../fine-tuned-model/{}'.format(model_name_arg)
+real_model_path = os.path.join(output_model_dir, ckpt_dir)
 
 result_dir = '../generated_result/'
-result_file_path = os.path.join(result_dir, 'result_from_{}.txt'.format(model_name))
+result_file_path = os.path.join(result_dir, 'result_from_{}.txt'.format(model_name_arg))
 
 os.makedirs(result_dir, exist_ok=True)
 
-
+max_output_len = 64
 
 if model_name == '':
     print('wrong model name.')
@@ -46,36 +52,73 @@ if model_name == '':
 dataset = load_dataset('csv', data_files={'test': os.path.join(data_dir,'test.csv')})
 
 
+dataloader = DataLoader(dataset, batch_size=4)
+
 ############## load tokenizer ##############
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
 
-
-############## load model for training ##############
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit=True
 )
-model = PeftModel.from_pretrained(model, real_model_path)
-model = model.merge_and_unload()
 
-############## generate results from model ##############
+if model_name_arg in ['llama2-7b', 'mistral-7b']:
+    print('loading LLM')
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16
+    )
 
-pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=64)
+    model = PeftModel.from_pretrained(model, real_model_path)
+    model = model.merge_and_unload()
+
+else:
+    print('loading encoder-decoder pre-trained model')
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        real_model_path,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16,
+        quantization_config=bnb_config
+    )
 
 generated_title_list = []
 
-for d in dataset:
-    input_txt = d['text']
 
-    result = pipe("<s>[INST] {} [/INST]".format(input_txt))
-    generated_title = result[0]['generated_text']
 
-    generated_title_list.append(generated_title)
+## continue from here...
+for batch in tqdm(dataloader):
+    input_encodings = tokenizer.batch_encode_plus(batch, pad_to_max_length=True, max_length=512)
+    input_ids = input_encodings['input_ids'], 
+    attention_mask = input_encodings['attention_mask']
+
+    outs = model.generate(
+        input_ids=batch['input_ids'], 
+        attention_mask=batch['attention_mask'],
+        max_length=64,
+        early_stopping=True)
+    outs = [tokenizer.decode(ids) for ids in outs]
+
+    generated_title_list.extend(outs)
+
+# # pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=64)
+
+
+# for d in dataset:
+#     input_txt = d['text']
+
+#     result = pipe("<s>[INST] {} [/INST]".format(input_txt))
+#     generated_title = result[0]['generated_text']
+
+#     generated_title_list.append(generated_title)
+
+
+#%%
 
 
 with open(result_file_path, 'w') as f:
--- Medium-Articles-title-generation/script/train.py
+++ Medium-Articles-title-generation/script/train.py
@@ -6,7 +6,7 @@
 import os, argparse
 
 import torch
-from transformers import AutoModelForCausalLM, TrainingArguments, AutoTokenizer, EarlyStoppingCallback, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, TrainingArguments, AutoTokenizer, EarlyStoppingCallback, BitsAndBytesConfig, Seq2SeqTrainer, AutoModelForSeq2SeqLM
 
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from trl import SFTTrainer
@@ -26,7 +26,9 @@ args = parser.parse_args()
 
 model_names = {
     'llama2-7b': 'meta-llama/Llama-2-7b-hf',
-    'mistral-7b': 'mistralai/Mistral-7B-v0.3'
+    'mistral-7b': 'mistralai/Mistral-7B-v0.3',
+    't5': 'google-t5/t5-3b',
+    'bart': 'facebook/bart-large'
 }
 
 data_dir = '../dataset/cleaned/'
@@ -47,58 +49,23 @@ dataset = load_dataset('csv', data_files={'train': os.path.join(data_dir,'train.
 ## just for testing
 # dataset = load_dataset('csv', data_files={'train': os.path.join(data_dir,'train.csv'), 'valid': os.path.join(data_dir,'valid_for_testing.csv')})
 
+
 print(dataset)
-
-def preprocess_function(examples):
-    
-    inputs = examples['text']
-    targets = examples['title']
-    
-    examples['text'] = '<s>[INST] {} [/INST] {} </s>'.format(inputs, targets)
-
-    return examples
-
-
-dataset = dataset.map(preprocess_function)
 
 
 ############## load tokenizer ##############
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+
 
 
 ############## load model for training ##############
-
-peft_config = LoraConfig(
-    task_type="CAUSAL_LM", 
-    inference_mode=False, 
-    r=16, 
-    lora_alpha=32, 
-    lora_dropout=0.1,
-    bias="none",
-    target_modules=["q_proj","k_proj","v_proj","o_proj"],
-    use_dora=True
-)
-
-compute_dtype = getattr(torch, "float16")
-
 
 bnb_config = BitsAndBytesConfig(
     load_in_8bit=True
 )
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    low_cpu_mem_usage=True,
-    return_dict=True,
-    torch_dtype=torch.float16,
-    quantization_config=bnb_config
-)
-
-model = prepare_model_for_kbit_training(model)
-model = get_peft_model(model, peft_config)
 
 train_batch_size = 8
 eval_batch_size = 8
@@ -131,17 +98,117 @@ training_args = TrainingArguments(
 )
 
 
-trainer = SFTTrainer(
-    model=model,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["valid"],
-    # peft_config=peft_config,
-    dataset_text_field="text",
-    max_seq_length=1024,
-    tokenizer=tokenizer,
-    args=training_args,
-    packing=False,
-    callbacks = [EarlyStoppingCallback(early_stopping_patience=3, early_stopping_threshold = 0.01)]
-)
 
-trainer.train()
+
+#%%
+
+def train_LLM():
+
+    def preprocess_function(examples):
+        
+        inputs = examples['text']
+        targets = examples['title']
+        
+        examples['text'] = '<s>[INST] {} [/INST] {} </s>'.format(inputs, targets)
+
+        return examples
+
+    tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+
+    dataset = dataset.map(preprocess_function)
+
+    peft_config = LoraConfig(
+        task_type="CAUSAL_LM", 
+        inference_mode=False, 
+        r=16, 
+        lora_alpha=32, 
+        lora_dropout=0.1,
+        bias="none",
+        target_modules=["q_proj","k_proj","v_proj","o_proj"],
+        use_dora=True
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16,
+        quantization_config=bnb_config
+    )
+
+    model = prepare_model_for_kbit_training(model)
+
+    model = get_peft_model(model, peft_config)
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["valid"],
+        # peft_config=peft_config,
+        dataset_text_field="text",
+        max_seq_length=1024,
+        tokenizer=tokenizer,
+        args=training_args,
+        packing=False,
+        callbacks = [EarlyStoppingCallback(early_stopping_patience=3, early_stopping_threshold = 0.01)]
+    )
+
+
+    trainer.train()
+
+
+def train_enc_dec_model():
+    def add_eos_to_examples(example):
+        example['text'] = '<s> {} </s>'.format(example['text'])
+        example['title'] = '<s> {} </s>'.format(example['title'])
+        return example
+
+    # tokenize the examples
+    def convert_to_features(example_batch):
+        input_encodings = tokenizer.batch_encode_plus(example_batch['text'], pad_to_max_length=True, max_length=512)
+        target_encodings = tokenizer.batch_encode_plus(example_batch['title'], pad_to_max_length=True, max_length=64)
+
+        encodings = {
+            'input_ids': input_encodings['input_ids'], 
+            'attention_mask': input_encodings['attention_mask'],
+            'target_ids': target_encodings['input_ids'],
+            'target_attention_mask': target_encodings['attention_mask']
+        }
+
+        return encodings
+
+    dataset = dataset.map(add_eos_to_examples)
+    dataset = dataset.map(convert_to_features, batched=True)
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_name,
+        low_cpu_mem_usage=True,
+        return_dict=True,
+        torch_dtype=torch.float16,
+        quantization_config=bnb_config
+    )
+
+    model = prepare_model_for_kbit_training(model)
+
+    trainer = Seq2SeqTrainer(
+        model=model,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["valid"],
+        max_seq_length=1024,
+        tokenizer=tokenizer,
+        args=training_args,
+        callbacks = [EarlyStoppingCallback(early_stopping_patience=3, early_stopping_threshold = 0.01)]
+    )
+
+    trainer.train()
+
+
+#%%
+
+if model_name_arg in ['llama2-7b', 'mistral-7b']:
+    print('training LLM')
+    train_LLM()
+
+else:
+    print('training encoder-decoder pre-trained model')
+    train_enc_dec_model()
