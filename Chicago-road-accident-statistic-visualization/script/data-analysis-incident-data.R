d3c713f8b56ebe943ca1b9f7b42d6e8a1c7340e1
Oat <windowsandandroid.app@gmail.com>
2024-08-13 23:19:16 +1000

    new code and figures

--- Chicago-road-accident-statistic-visualization/script/data-analysis-incident-data.R
+++ Chicago-road-accident-statistic-visualization/script/data-analysis-incident-data.R
@@ -3,6 +3,225 @@ library(tidyverse)
 options(scipen=1000000)
 theme_set(theme_bw())
 
-df = read.csv('../dataset/cleaned/cleaned_person_data.csv')
+df = read.csv('../dataset/cleaned/cleaned_incident_data.csv')
 
 fig.dir = '../figure/'
+
+df = df %>% 
+  rename(
+    Month = CRASH_MONTH,
+    day_of_week = CRASH_DAY_OF_WEEK
+  ) %>%
+  mutate(
+    Month = month.abb[Month],
+    Month = factor(Month, levels = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")),
+    day_of_week = wday(day_of_week, label = T),
+    Speed.limit.range = case_when(
+      POSTED_SPEED_LIMIT <= 20 ~ '< 20',
+      POSTED_SPEED_LIMIT > 20 & POSTED_SPEED_LIMIT <= 40 ~ '21-40',
+      POSTED_SPEED_LIMIT > 40 & POSTED_SPEED_LIMIT <= 60 ~ '41-60',
+      POSTED_SPEED_LIMIT > 60 & POSTED_SPEED_LIMIT <= 80 ~ '61-80',
+      POSTED_SPEED_LIMIT > 80  ~ '80+',
+    ),
+    Speed.limit.range = factor(Speed.limit.range, levels=c('< 20', '21-40', '41-60', '61-80', '80+')),
+    WORK_ZONE_TYPE = if_else(
+      WORK_ZONE_TYPE == 'UNKNOWN', 'NOT-AT-WORK-ZONE', WORK_ZONE_TYPE),
+    INJURIES_FATAL = as.numeric(str_replace(INJURIES_FATAL, 'UNKNOWN','0')),
+        INJURIES_INCAPACITATING = as.numeric(str_replace(INJURIES_INCAPACITATING, 'UNKNOWN','0')),
+        INJURIES_NON_INCAPACITATING = as.numeric(str_replace(INJURIES_NON_INCAPACITATING, 'UNKNOWN','0')),
+        INJURIES_REPORTED_NOT_EVIDENT = as.numeric(str_replace(INJURIES_REPORTED_NOT_EVIDENT, 'UNKNOWN','0'))
+  )
+  
+
+save.fig = function(fig.name, scale=1){
+  ggsave(paste0(fig.dir, fig.name,'.png'), scale=scale)
+}
+
+
+
+## num accidents by weather and month
+
+new.df = df %>% 
+  filter(WEATHER_CONDITION != 'UNKNOWN') %>%
+  count(Month, WEATHER_CONDITION)
+  # mutate(n = log10(n))
+
+new.df %>% ggplot(aes(x=Month, y=WEATHER_CONDITION, fill=n)) + 
+  geom_tile() + 
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) +
+  labs(
+    title = 'Number of accidents in each month by weather condition',
+    y = 'Weather Condition',
+    fill = 'Count'
+  )
+  # theme(legend.position = 'bottom')
+
+save.fig('num_accidents_each_month_by_weather')
+
+
+
+## num accidents by day of week and month
+
+new.df = df %>%
+  group_by(Month, day_of_week) %>%
+  count()
+
+new.df %>% ggplot(aes(x=Month, y=n, color=day_of_week, group=day_of_week)) + 
+  geom_line() +
+  geom_point() +
+  labs(
+    title = 'Number of accidents in each month by day of week',
+    y = 'Count'
+  ) + 
+  theme(legend.position = 'bottom') +
+  scale_color_manual(values = c('red', 'yellow', 'pink', 'green', 'orange', 'blue', 'purple'))
+
+save.fig('num_accidents_by_day_of_week')
+
+
+
+## top-10 contribution cause
+
+new.df = df %>%
+  count(PRIM_CONTRIBUTORY_CAUSE) %>%
+  arrange(desc(n)) %>%
+  head(10)
+
+top.10.cause = new.df$PRIM_CONTRIBUTORY_CAUSE
+
+new.df %>% ggplot(aes(y=reorder(PRIM_CONTRIBUTORY_CAUSE, n), x=n, fill=PRIM_CONTRIBUTORY_CAUSE)) +
+  geom_bar(stat = 'identity') +
+  theme(legend.position = 'none') +
+  geom_text(aes(label=n, hjust = 0.7)) +
+  labs(
+    title = 'Top-10 causes of accidents', 
+    x = 'Count',
+    y = 'Primary Contribution Cause'
+  )
+
+save.fig('top_10_accidents_causes')
+
+
+
+## top-10 first crash type
+
+new.df = df %>%
+  count(FIRST_CRASH_TYPE) %>%
+  arrange(desc(n)) %>%
+  head(10)
+
+top.10.crash.type = new.df$FIRST_CRASH_TYPE
+
+new.df %>% ggplot(aes(y=reorder(FIRST_CRASH_TYPE, n), x=n, fill=FIRST_CRASH_TYPE)) +
+  geom_bar(stat = 'identity') +
+  theme(legend.position = 'none') +
+  geom_text(aes(label=n, hjust = 0.7)) +
+  labs(
+    title = 'Top-10 crash type', 
+    x = 'Count',
+    y = 'Crash Type'
+  )
+
+save.fig('top_10_crash_type')
+
+
+
+## number of different injury types in each month
+
+new.df = df %>%
+  select(INJURIES_FATAL, INJURIES_INCAPACITATING, INJURIES_NON_INCAPACITATING, INJURIES_REPORTED_NOT_EVIDENT, Month) %>%
+  group_by(Month) %>%
+  summarise(across(everything(), sum))
+
+new.df = new.df %>%
+  pivot_longer(
+    cols = starts_with('INJ'),
+    names_to = 'Injury_Type'
+      ) %>%
+  mutate(Injury_Type = str_replace(Injury_Type, 'INJURIES_',''))
+
+new.df %>% ggplot(aes(x=Month, y = value, color = Injury_Type, group = Injury_Type)) + 
+  geom_line() + 
+  geom_point() +
+  labs(
+    title = 'Number of people at crash site in each month by injury type',
+    y = 'Count',
+    color = 'Injury Type'
+  )
+
+save.fig('num_injured_people_each_month_by_injury_type')
+
+
+
+## number of accidents in each month by surroundings in crash site
+
+new.df = df %>%
+  select(INTERSECTION_RELATED_I, NOT_RIGHT_OF_WAY_I, HIT_AND_RUN_I, DOORING_I, WORK_ZONE_I, Month) %>%
+  pivot_longer(!Month, names_to='Surrounding_Condition') %>%
+  filter(value == 'Y') %>%
+  count(Month, Surrounding_Condition) %>%
+  mutate(Surrounding_Condition = str_replace(Surrounding_Condition, '_I',''))
+
+new.df %>% ggplot(aes(x=Month, y = n, color = Surrounding_Condition, group = Surrounding_Condition)) + 
+  geom_line() + 
+  geom_point() +
+  labs(
+    title = 'Total accidents in each month by surrounding situation',
+    y = 'Count',
+    color = 'Surrounding Situation'
+  )
+
+save.fig('num_accidents_each_month_by_situation')
+
+
+
+## number of accidents by crash type and roadway surface condition
+
+new.df = df %>%
+  filter(ROADWAY_SURFACE_COND != 'UNKNOWN') %>%
+  count(FIRST_CRASH_TYPE, ROADWAY_SURFACE_COND) %>%
+  mutate(
+    ROADWAY_SURFACE_COND = factor(ROADWAY_SURFACE_COND, levels=c('DRY', 'ICE', 'WET', 'SAND, MUD, DIRT', 'SNOW OR SLUSH', 'OTHER'))
+  )
+
+
+
+new.df %>% ggplot(aes(x=n, y = reorder(FIRST_CRASH_TYPE, n), fill=ROADWAY_SURFACE_COND)) + 
+  geom_bar(stat = 'identity', position = 'fill') + 
+  labs(
+    title = 'Number of accident by crash type and roadway surface condition',
+    x = 'Ratio',
+    y = 'Crash Type',
+    fill = 'Roadway Surface Condition'
+  )
+
+save.fig('num_accident_by_crash_type_and_roadway_surface')  
+
+
+
+## number of different road defect in each month
+
+new.df = df %>%
+  select(INJURIES_FATAL, INJURIES_INCAPACITATING, INJURIES_NON_INCAPACITATING, INJURIES_REPORTED_NOT_EVIDENT, ROAD_DEFECT) %>%
+  filter(ROAD_DEFECT != 'UNKNOWN') %>%
+  group_by(ROAD_DEFECT) %>%
+  summarise(across(everything(), sum))
+
+new.df = new.df %>%
+  pivot_longer(
+    cols = starts_with('INJ'),
+    names_to = 'Injury_Type'
+  ) %>%
+  mutate(Injury_Type = str_replace(Injury_Type, 'INJURIES_',''))
+
+new.df %>% ggplot(aes(x=value, y = reorder(ROAD_DEFECT, value), fill=Injury_Type)) + 
+  geom_bar(stat = 'identity', position = 'fill') + 
+  labs(
+    title = 'Number of accident by crash type and roadway surface condition',
+    x = 'Ratio',
+    y = 'Crash Type',
+    fill = 'Roadway Surface Condition'
+  ) + 
+  theme(legend.position = 'bottom')
+
+save.fig('num_injured_people_each_month_by_injury_type')
--- Chicago-road-accident-statistic-visualization/script/data-analysis-person-data.R
+++ Chicago-road-accident-statistic-visualization/script/data-analysis-person-data.R
@@ -8,13 +8,224 @@ df = read.csv('../dataset/cleaned/cleaned_person_data.csv')
 fig.dir = '../figure/'
 
 df = df %>%
-  rename(
-    Sex = SEX,
-    Age = AGE
-  ) %>%
-  mutate(Sex = recode(Sex,
-                      'F' = 'Female',
-                      'M' = 'Male',
-                      'X' = 'Other'
+  rename(Sex = SEX, Age = AGE) %>%
+  mutate(
+    Sex = recode(Sex, 'F' = 'Female', 'M' = 'Male', 'X' = 'Other'),
+    Age.range = case_when(
+      Age  == 'UNKNOWN' ~ 'UNKNOWN',
+      Age <= 10 ~ '<= 10',
+      Age > 10 & Age <= 20 ~ '11-20',
+      Age > 20 & Age <= 30 ~ '21-30',
+      Age > 30 & Age <= 40 ~ '31-40',
+      Age > 40 & Age <= 50 ~ '41-50',
+      Age > 50 & Age <= 60 ~ '51-60',
+      Age > 60 ~ '60+'
+    ),
+   Age.range = factor(Age.range, levels=c('<= 10', '11-20', '21-30', '31-40', '41-50', '51-60', '60+', 'UNKNOWN')),
+   INJURY_CLASSIFICATION = factor(INJURY_CLASSIFICATION),
+   INJURY_CLASSIFICATION = fct_recode(INJURY_CLASSIFICATION,
+      'FATAL' = 'FATAL',
+      'INCAPACITATING' = 'INCAPACITATING INJURY',
+      'NO INJURY' = 'NO INDICATION OF INJURY',
+      'NONINCAPACITATING' = 'NONINCAPACITATING INJURY',
+      'NOT EVIDENT' = 'REPORTED, NOT EVIDENT',
+      'UNKNOWN' = 'UNKNOWN'
+      ),
+  SAFETY_EQUIPMENT = factor(SAFETY_EQUIPMENT),
+  SAFETY_EQUIPMENT = fct_recode(SAFETY_EQUIPMENT,
+         "UNKNOWN" = "USAGE UNKNOWN",
+         "UNKNOWN" = "UNKNOWN",
+         "SAFETY BELT" = "SAFETY BELT USED",
+         "NONE" = "SAFETY BELT NOT USED",
+         "NONE" = "NONE PRESENT",
+         "NONE" = "HELMET NOT USED",
+         "HELMET" = "BICYCLE HELMET (PEDACYCLIST INVOLVED ONLY)",
+         "HELMET" = "HELMET USED",
+         "HELMET" = "DOT COMPLIANT MOTORCYCLE HELMET",
+         "HELMET" = "NOT DOT COMPLIANT MOTORCYCLE HELMET",
+         "CHILD RESTRAINT" = "CHILD RESTRAINT - REAR FACING",
+         "CHILD RESTRAINT" = "CHILD RESTRAINT - TYPE UNKNOWN",
+         "CHILD RESTRAINT" = "CHILD RESTRAINT - FORWARD FACING",
+         "NONE" = "CHILD RESTRAINT NOT USED",
+         "BOOSTER SEAT" = "BOOSTER SEAT",
+         "IMPROPER EQUIPEMENT USED" = "SHOULD/LAP BELT USED IMPROPERLY",
+         "IMPROPER EQUIPEMENT USED" = "CHILD RESTRAINT USED IMPROPERLY",
+         "WHEELCHAIR" = "WHEELCHAIR",
+         "STRETCHER" = "STRETCHER"
+         ),
+  AIRBAG_DEPLOYED = recode(AIRBAG_DEPLOYED,
+      "DEPLOYMENT UNKNOWN" = "UNKNOWN",
+      "DID NOT DEPLOY" = "NO DEPLOYMENT",
+      "NOT APPLICABLE" = "NOT APPLICABLE",
+      "UNKNOWN" = "NOT APPLICABLE",
+      "DEPLOYED, FRONT" = "DEPLOYED",
+      "DEPLOYED, COMBINATION" = "DEPLOYED",
+      "DEPLOYED, SIDE" = "DEPLOYED",   
+      "DEPLOYED OTHER (KNEE, AIR, BELT, ETC.)" = "DEPLOYED"
+      ),
+  DRIVER_VISION = recode(DRIVER_VISION,
+      "MOVING VEHICLES" = "VEHICLES",
+      "PARKED VEHICLES" = "VEHICLES",
+      "BLINDED - SUNLIGHT" = "BLINDED",
+      "BLINDED - HEADLIGHTS" = "BLINDED",
+      "IMPROPER LANE CHANGE" = "IMPROPER DRIVING",
+      "WRONG WAY/SIDE" = "IMPROPER DRIVING",
+      "IMPROPER TURN" = "IMPROPER DRIVING",
+      "IMPROPER TURN" = "IMPROPER DRIVING"
+      ),
+  PEDPEDAL_ACTION_TYPE = recode(PEDPEDAL_ACTION,
+      "CROSSING - NO CONTROLS (AT INTERSECTION)" = 'CROSSING',
+      "CROSSING - NO CONTROLS (NOT AT INTERSECTION)" = 'CROSSING',
+      "CROSSING - WITH SIGNAL" = "CROSSING",
+      "CROSSING - AGAINST SIGNAL" = "CROSSING",
+      "TURNING RIGHT" = "CHANGE DIRECTION",
+      "STANDING IN ROADWAY" = "IN ROADWAY",
+      "TURNING RIGHT" = "CHANGE DIRECTION",
+      "PLAYING IN ROADWAY" = "IN ROADWAY",
+      "WORKING IN ROADWAY" = "IN ROADWAY",
+      "CROSSING - CONTROLS PRESENT (NOT AT INTERSECTION)" = "CROSSING",
+      "TO/FROM DISABLED VEHICLE" = "IN VEHICLE",
+      "PLAYING/WORKING ON VEHICLE" = "IN VEHICLE"
     )
   )
+
+
+save.fig = function(fig.name, scale=1){
+  ggsave(paste0(fig.dir, fig.name,'.png'), scale=scale)
+}
+
+
+
+## age range vs gender
+
+new.df = df %>% 
+  filter(!Sex %in% c('UNKNOWN', 'Other') & Age.range != 'UNKNOWN') %>%
+  count(Age.range, Sex)
+  
+new.df %>% ggplot(aes(x=Age.range, y=n, fill=Sex)) + 
+  geom_bar(stat = 'identity', position = 'dodge') + 
+  labs(
+    title = 'Number of people that involved in accidents by age and gender',
+    x = 'Age Range',
+    y = 'Count'
+  ) + 
+  theme(legend.position = 'bottom')
+
+save.fig('num_people_by_age_and_gender')
+
+
+
+## PEDPEDAL_ACTION vs PEDPEDAL_LOCATION
+
+new.df = df %>%
+  filter(
+    !PEDPEDAL_ACTION_TYPE %in% c('UNKNOWN', "UNKNOWN/NA") &
+      !PEDPEDAL_LOCATION %in% c('UNKNOWN', "UNKNOWN/NA")
+  ) %>%
+  count(PEDPEDAL_ACTION_TYPE, PEDPEDAL_LOCATION)
+
+new.df %>% ggplot(aes(x=PEDPEDAL_LOCATION, y=PEDPEDAL_ACTION_TYPE, fill=n)) + 
+  geom_tile() + 
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) + 
+  labs(
+    title = 'Number of pedestraints that involved in accidents by action and location',
+    x = 'Pedpedal Location',
+    y = 'Pedpedal Action',
+    fill = ''
+  )
+
+save.fig('num_pedpedal_by_action_and_location')
+
+
+
+## INJURY_CLASSIFICATION vs AIRBAG_DEPLOYED
+
+new.df = df %>%
+  filter(INJURY_CLASSIFICATION != 'UNKNOWN' & 
+           AIRBAG_DEPLOYED %in% c("NO DEPLOYMENT", "DEPLOYED") &
+           PERSON_TYPE %in% c('DRIVER', 'PASSENGER')) %>%
+  count(INJURY_CLASSIFICATION, AIRBAG_DEPLOYED) %>%
+  mutate(n = log10(n))
+
+new.df %>% ggplot(aes(x=INJURY_CLASSIFICATION, y=n, fill = AIRBAG_DEPLOYED)) + 
+  geom_bar(stat = 'identity', position = 'dodge') +
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) + 
+  labs(
+    title = 'Number of people that involved in accidents by injury type and airbag deployment',
+    x = 'Injury Type',
+    y = 'Count (log base 10)',
+    fill = 'Airbag Deployment'
+  ) 
+  # theme(legend.position = 'bottom')
+
+save.fig('num_people_by_injury_and_airbag')
+
+
+
+## INJURY_CLASSIFICATION vs SAFETY_EQUIPMENT
+
+new.df = df %>%
+  filter(INJURY_CLASSIFICATION != 'UNKNOWN' & 
+           SAFETY_EQUIPMENT != 'UNKNOWN') %>%
+  count(INJURY_CLASSIFICATION, SAFETY_EQUIPMENT)
+  # mutate(n = log10(n))
+
+new.df %>% ggplot(aes(x=n, y=SAFETY_EQUIPMENT, fill = INJURY_CLASSIFICATION)) + 
+  geom_bar(stat = 'identity', position = 'fill') +
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) + 
+  labs(
+    title = 'Number of people that involved in accidents by injury type and safety equipement',
+    x = 'Count',
+    y = 'Safety Equipement',
+    fill = 'Injury Type'
+  )
+  # theme(legend.position = 'bottom')
+
+save.fig('num_people_by_injury_and_safety_equipment')
+
+
+
+## INJURY_CLASSIFICATION vs ejection
+
+new.df = df %>%
+  filter(INJURY_CLASSIFICATION != 'UNKNOWN' & 
+           !EJECTION %in% c('UNKNOWN', 'NONE')) %>%
+  count(INJURY_CLASSIFICATION, EJECTION)
+  # mutate(n = log10(n))
+
+new.df %>% ggplot(aes(x=INJURY_CLASSIFICATION, y=n, fill = EJECTION)) + 
+  geom_bar(stat = 'identity', position = 'dodge') +
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) + 
+  labs(
+    title = 'Number of people that involved in accidents by injury type and ejection status',
+    x = 'Injury Type',
+    y = 'Count',
+    fill = 'Ejection Status'
+  ) +
+  theme(legend.position = 'bottom')
+
+save.fig('num_people_by_injury_and_ejection')
+
+
+
+## INJURY_CLASSIFICATION vs driver vision
+
+new.df = df %>%
+  filter(INJURY_CLASSIFICATION != 'UNKNOWN' & 
+           DRIVER_VISION != 'UNKNOWN' & 
+           PERSON_TYPE == 'DRIVER') %>%
+  count(INJURY_CLASSIFICATION, DRIVER_VISION)
+  # mutate(n = log10(n))
+
+new.df %>% ggplot(aes(x=n, y=DRIVER_VISION, fill = INJURY_CLASSIFICATION)) + 
+  geom_bar(stat = 'identity', position = 'fill') +
+  theme(axis.text.x = element_text(angle=15, vjust = 0.5)) + 
+  labs(
+    title = 'Number of driver that involved in accidents by injury type and vision',
+    x = 'Count',
+    y = 'Vision Type',
+    fill = 'Injury Type'
+  )
+  # theme(legend.position = 'bottom')
+
+save.fig('num_driver_by_injury_and_vision')
